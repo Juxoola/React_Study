eb8c128d5f0d19dc8aaca0920d4e709bf0ce2eca
Juxoola <liketoeatpizza19@gmail.com>
2024-11-26 06:25:45 +0300

    4

--- vite-project-intr/src/App.jsx
+++ vite-project-intr/src/App.jsx
@@ -1,10 +1,28 @@
 import Header from "./components/Header"
+import { menu } from "./data"
+function MenuItem(props) {
+  console.log(props)
+  return(
+    <li>
+      <p>
+        <strong>{props.title}</strong> {props.description}
+      </p>
+    </li>
+  )
+  
+}
 export default function App() {
   return(
     <div>
       <Header></Header>
       <main>
-        <h1>hello world!</h1>
+        <section>
+          <h3>Menu</h3>
+          <ul>
+            <MenuItem title="Breakfast" description="Bla bla"/>
+            <MenuItem title="Breakfast 33" description="Bla bla"/>
+          </ul>
+        </section>
       </main>
     </div>
   )
---
+++ vite-project-intr/src/data.js
@@ -0,0 +1,18 @@
+export const menu = [
+    {
+    title: 'Breakfast',
+    description: 'Breakfast is the most important meal of the day.',
+    },
+    {
+        title: 'Lunch',
+        description: 'Lunch is a meal typically eaten around midday.',
+    },
+    {
+        title: 'Dinner',
+        description: 'Dinner is the main meal of the day, usually taken in the evening.',
+    },
+    {
+        title: 'Snack',
+        description: 'A snack is a small portion of food generally eaten between meals.',
+    },
+  ];
