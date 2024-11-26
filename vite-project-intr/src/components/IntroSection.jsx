e7946e0664ecced3b4f4ff33da69e285a7cfecb7
Juxoola <liketoeatpizza19@gmail.com>
2024-11-26 11:59:46 +0300

    Oi

--- vite-project-intr/src/components/IntroSection.jsx
+++ vite-project-intr/src/components/IntroSection.jsx
@@ -10,6 +10,6 @@
 // import React from "react";
 
 // export default function IntroSection() {
-//     return React.createElement('section',null,[ React.createElement('h1', {className: 'centered'}, 'Добро пожаловать'), 
-//         React.createElement('h3',  {className: 'centered', style:{color: 'red'}} , 'Зачем зашли на сайт?')])
+//     return React.createElement('section',null,[ React.createElement('h1', {className: 'centered' key: '1'}, 'Добро пожаловать'), 
+//         React.createElement('h3',  {className: 'centered', style:{color: 'red'} key: '2'} , 'Зачем зашли на сайт?')])
 // }
