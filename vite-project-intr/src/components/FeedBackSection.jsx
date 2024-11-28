a5665f461aa5f19a49a286b3eb225e72fa1611a0
Juxoola <liketoeatpizza19@gmail.com>
2024-11-28 11:54:08 +0300

    Форма

--- vite-project-intr/src/components/FeedBackSection.jsx
+++ vite-project-intr/src/components/FeedBackSection.jsx
@@ -1,7 +1,21 @@
+import Button from "./Button/Button"
 export default function FeedBackSection() {
     return (
         <section>
             <h3>Обратная связь</h3>
+            <form>
+                <label htmlFor="name">Ваше имя</label>
+                <input type="text" id= "name" className="control" />
+                
+                <label htmlFor="reason">Причина оьбращения</label>
+                <select id="reason" className="control">
+                    <option value="error">Ошибка</option>
+                    <option value="help">Нужна помощь</option>
+                    <option value="suggest">Предложение</option>
+                </select>
+                    
+                <Button>Отправить</Button>
+            </form>
         </section>
     )
 }
