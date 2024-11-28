0bb05fa69fc9cf33a79456fe2bdbcfda7a794700
Juxoola <liketoeatpizza19@gmail.com>
2024-11-28 14:52:56 +0300

    Кастомный хук (поиск)

--- vite-project-intr/src/components/EffectsSection.jsx
+++ vite-project-intr/src/components/EffectsSection.jsx
@@ -1,8 +1,11 @@
+import useInput from '../hooks/userInput'
 import Button from './Button/Button'
 import Modal from './Modal/Modal'
 import { useEffect, useState, useCallback } from 'react'
 
 export default function EffectsSection() {
+	const input = useInput()
+
 	const [modal, setModal] = useState(false)
 	const [loading, setLoading] = useState(false)
 	const [users, setUsers] = useState([])
@@ -15,7 +18,6 @@ export default function EffectsSection() {
 		setLoading(false)
 	}, [])
 
-
 	useEffect(() => {
 		fetchData()
 	}, [fetchData])
@@ -24,7 +26,9 @@ export default function EffectsSection() {
 		<section>
 			<h3>Effects</h3>
 
-			<Button style={{ marginBottom: '1rem' }} onClick={() => setModal(true)}>Открыть информацию</Button>
+			<Button style={{ marginBottom: '1rem' }} onClick={() => setModal(true)}>
+				Открыть информацию
+			</Button>
 
 			<Modal open={modal}>
 				<h3>Modal</h3>
@@ -34,11 +38,18 @@ export default function EffectsSection() {
 
 			{loading && <p>Загрузка...</p>}
 			{!loading && (
-				<ul>
-					{users.map(user => (
-						<li key={user.id}>{user.name}</li> 
-					))}
-				</ul>
+				<>
+					<input type='text' className='control' {...input} />
+					<ul>
+						{users
+							.filter(user =>
+								user.name.toLowerCase().includes(input.value.toLowerCase())
+							)
+							.map(user => (
+								<li key={user.id}>{user.name}</li>
+							))}
+					</ul>
+				</>
 			)}
 		</section>
 	)
