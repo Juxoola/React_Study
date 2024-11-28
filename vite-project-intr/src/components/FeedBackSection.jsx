5fb5b512f3f0397c682dae62542c8fb45adcae58
Juxoola <liketoeatpizza19@gmail.com>
2024-11-28 12:00:57 +0300

    Set Reason and Name

--- vite-project-intr/src/components/FeedBackSection.jsx
+++ vite-project-intr/src/components/FeedBackSection.jsx
@@ -1,18 +1,32 @@
+import { useState } from "react"
 import Button from "./Button/Button"
 export default function FeedBackSection() {
+    const [name, setName] = useState('')
+    const [reason, setReason] = useState('help')
+
+    function handleNameChange(event) {
+        setName(event.target.value)
+    }
+
     return (
         <section>
             <h3>Обратная связь</h3>
             <form>
                 <label htmlFor="name">Ваше имя</label>
-                <input type="text" id= "name" className="control" />
+                <input type="text" id= "name" className="control" value={name} onChange={handleNameChange}/>
                 
                 <label htmlFor="reason">Причина оьбращения</label>
-                <select id="reason" className="control">
+                <select id="reason" className="control" value={reason} onChange={event => setReason(event.target.value)}>
                     <option value="error">Ошибка</option>
                     <option value="help">Нужна помощь</option>
                     <option value="suggest">Предложение</option>
                 </select>
+
+                <pre>
+                    Name: {name}
+                    <br />
+                    Reason {reason}
+                </pre>
                     
                 <Button>Отправить</Button>
             </form>
