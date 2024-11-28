abd8c7ab3a030d5b26b18333470f45ce90023b94
Juxoola <liketoeatpizza19@gmail.com>
2024-11-28 14:02:09 +0300

    Modal

--- vite-project-intr/src/App.jsx
+++ vite-project-intr/src/App.jsx
@@ -5,32 +5,32 @@ import IntroSection from "./components/IntroSection"
 import TabSection from "./components/TubSection"
 import FeedBackSection from "./components/FeedBackSection"
 import { useState } from "react"
+import EffectsSection from "./components/EffectsSection"
 
 export default function App() {
 
-  const [tab, setTab] = useState('feedback')
+  const [tab, setTab] = useState('effect')
 
-  return(
-    <>
-      <Header></Header>
-      <main>
-        <IntroSection/>
-        <TabSection active={tab} onChange={current => setTab(current)}/>
+  return (
+		<>
+			<Header></Header>
+			<main>
+				<IntroSection />
+				<TabSection active={tab} onChange={current => setTab(current)} />
 
-        {tab === 'main' && (
-          <> 
-            <MenuSection/>
-            <DifSection/>
-          </>
-        )}
+				{tab === 'main' && (
+					<>
+						<MenuSection />
+						<DifSection />
+					</>
+				)}
 
-        {tab === 'feedback' && (
-            <FeedBackSection/>
-        )}  
-       
-      </main>
-    </>
-  )
+				{tab === 'feedback' && <FeedBackSection />}
+
+				{tab === 'effect' && <EffectsSection />}
+			</main>
+		</>
+	)
 }
 
 
---
+++ vite-project-intr/src/components/EffectsSection.jsx
@@ -0,0 +1,26 @@
+import Button from './Button/Button'
+import Modal from './Modal/Modal'
+import { useState } from 'react'
+
+export default function EffectsSection() {
+  
+  const [modal, setModal] = useState(false)
+	
+  function openModal() {
+    setModal(true)
+  }
+
+	return (
+		<section>
+			<h3>Effects</h3>
+
+			<Button onClick={openModal}>Открыть информацию</Button>
+			
+			<Modal open={modal}>
+				<h3>Modal</h3>
+				<p>Lorem ipsum dolor sit amet consectetur adipisicing elit. </p>
+			</Modal>
+
+		</section>
+	)
+}
